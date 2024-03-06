e3ce7ab1c08040269b3954fb338e01bcb04fb8f9
U_Alice <umugwanezaalice22@gmail.com>
2024-03-06 22:51:29 +0200

    [feat]: more relationships

---
+++ src/main/java/rw/dyna/ecommerce/v1/models/WishList.java
@@ -0,0 +1,15 @@
+package rw.dyna.ecommerce.v1.models;
+
+import rw.dyna.ecommerce.v1.audits.InitiatorAudit;
+import javax.persistence.*;
+import java.util.UUID;
+
+@Entity
+@Table(name = "wishlist")
+public class WishList extends InitiatorAudit {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    @ManyToOne
+    private Client client;
+}
