31ec83fa115f2d521ea60f809aef8a3c2adde4c3
U_Alice <umugwanezaalice22@gmail.com>
2024-03-12 06:39:05 +0200

    [feat]: initializing controllers

--- src/main/java/rw/dyna/ecommerce/v1/controllers/DeliveryController.java
+++ src/main/java/rw/dyna/ecommerce/v1/controllers/DeliveryController.java
@@ -1,10 +1,42 @@
 package rw.dyna.ecommerce.v1.controllers;
 
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+import rw.dyna.ecommerce.v1.payloads.ApiResponse;
 
 @Controller
 @RequestMapping("api1/v1/delivery")
 public class DeliveryController {
+    @PostMapping("/create")
+    public ResponseEntity<ApiResponse> createClient(){
+        return null;
+    }
+    @GetMapping("/")
+    public ResponseEntity<ApiResponse> getAllClients(){
+        return null;
+    }
+
+    @GetMapping("/paginated")
+    public ResponseEntity<ApiResponse> getAllClientsPaginated(@PathVariable("limit") int limit, @PathVariable("page") int page){
+        Pageable pageable = PageRequest.of(page, limit);
+        return null;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ApiResponse> getClientById(){
+        return null;
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ApiResponse> getById(){
+        return null;
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ApiResponse> deleteById(){
+        return null;
+    }
 }
--- src/main/java/rw/dyna/ecommerce/v1/controllers/WishListController.java
+++ src/main/java/rw/dyna/ecommerce/v1/controllers/WishListController.java
@@ -1,4 +1,40 @@
 package rw.dyna.ecommerce.v1.controllers;
 
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import rw.dyna.ecommerce.v1.payloads.ApiResponse;
+
+@RequestMapping("/api/v1/wishList")
 public class WishListController {
+    @PostMapping("/create")
+    public ResponseEntity<ApiResponse> createClient(){
+        return null;
+    }
+    @GetMapping("/")
+    public ResponseEntity<ApiResponse> getAllClients(){
+        return null;
+    }
+
+    @GetMapping("/paginated")
+    public ResponseEntity<ApiResponse> getAllClientsPaginated(@PathVariable("limit") int limit, @PathVariable("page") int page){
+        Pageable pageable = PageRequest.of(page, limit);
+        return null;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ApiResponse> getClientById(){
+        return null;
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ApiResponse> getById(){
+        return null;
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ApiResponse> deleteById(){
+        return null;
+    }
 }
